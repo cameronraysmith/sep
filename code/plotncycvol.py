ce08830802a7319bcb207c2f3be2dc9ca55f68ef
Cameron Smith <camrn86@gmail.com>
2013-07-11 17:28:44 -0400

    increase fontsize on ncycle volume figure axes labels and move header of main tex file into a separate file

--- code/plotncycvol.py
+++ code/plotncycvol.py
@@ -20,22 +20,23 @@ def main(argv):
     rc('text', usetex=True)
     rc('font', family='serif')
 
-    fig1=plt.figure(1)
+    fig1=plt.figure(num=1,figsize=(12,9),facecolor='w')
     ax1f1 = fig1.add_subplot(111)
-    ax1f1.set_ylabel('non-modular:modular volume ratio',
-                     fontsize=16,labelpad=20,fontweight='normal')
-    ax1f1.set_xlabel('n',fontsize=16,labelpad=10)
+    ax1f1.set_ylabel('volume ratio (non-modular : modular)',
+                     fontsize=30,labelpad=20,fontweight='normal')
+    ax1f1.set_xlabel('n',fontsize=30,labelpad=10)
     ax1f1.set_xlim((2, 7))
     ax1f1.set_ylim((0.6, 1.1))
     ax1f1.set_xticklabels(('','3','4','5','6',''))
     ax1f1.set_yticklabels(('','','.7','','','1',''))
-    ax1f1.tick_params(axis='both', which='major', labelsize=16)
+    ax1f1.tick_params(axis='both', which='major', labelsize=30)
     plt.grid(True)
     f1p1 = plt.plot(l[:,1],l[:,0],
                     linestyle='-', linewidth=5, color='k',
                     marker='o',markersize=12,
                     mec='k',mfc='r')
-    plt.savefig(ffname)
+    plt.savefig(ffname,bbox_inches='tight',
+        facecolor=fig1.get_facecolor(), edgecolor='none')
     plt.close()
 
     vnc = subprocess.check_output(["evince",ffname])
