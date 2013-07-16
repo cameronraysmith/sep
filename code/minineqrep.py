818958697eb5206845f77965f39662249896ff44
Cameron Smith <camrn86@gmail.com>
2013-07-15 23:59:11 -0400

    extract Boole / totally consistent vertices via Minkowski-Weyl transformation on the H-representation of the Kolmogorov consistent polytope for arbitrary hypergraphs of interactions

--- code/minineqrep.py
+++ code/minineqrep.py
@@ -1,71 +1,150 @@
+"""
+minineqrep.py
+generate a minimal / full-dimensional representation
+of the inequalities / facets of the Kolmogorov consistent
+polytope and compute its volume
+"""
 import sys
 import numpy as np
 import sympy as sp
 import subprocess
 
 def redineq(eqfname):
+    """
+    input: filename containing Kolmogorov Consistency equalities
+           for some hypergraph
+    output: polrepindeineqs - polymake compatible minimal
+                              representation
+            poldim - expected dimension of the resulting polytope
+    """
     delimiter = ' '
 
-    fhand = open ( eqfname , 'r')
-    l1 = [ map(int,line.split(delimiter)) for line in fhand ]
-    l1 = np.array(l1)
+    fhand = open (eqfname , 'r')
+
+    #kceqsf = [ map(int,line.split(delimiter)) for line in fhand ]
+    kceqsf = [[int(x) for x in line.split(delimiter)]
+                for line in fhand ]
+    kceqsf = np.array(kceqsf)
     fhand.close()
 
-    l2 = np.zeros(shape=np.shape(l1), dtype=np.int)
-    l2[:, :-1] = l1[:, 1:]
-    l2[:, -1] = l1[:, 0]
+    kceqs = np.zeros(shape=np.shape(kceqsf), dtype=np.int)
+    kceqs[:, :-1] = kceqsf[:, 1:]
+    kceqs[:, -1] = kceqsf[:, 0]
 
-    ref, ind = sp.Matrix(-1*l2).rref(simplified=True)
-    refa = np.squeeze(np.asarray(ref))
+    kceqsref, ind = sp.Matrix(-1*kceqs).rref(simplified=True)
+    kceqsrefa = np.squeeze(np.asarray(kceqsref))
 
-    im1 = np.arange(refa.shape[1])
+    im1 = np.arange(kceqsrefa.shape[1])
     mask1 = np.ones(len(im1), dtype=bool)
     mask1[ind] = False
 
-    indineqs = refa[:, im1[mask1]]
+    indineqs = kceqsrefa[:, im1[mask1]]
     polrepindineqs = np.zeros(shape=np.shape(indineqs), dtype=np.int)
-    polrepindineqs[:,1:] = indineqs[:,:-1]
-    polrepindineqs[:,0] = indineqs[:, -1]
+    polrepindineqs[:, 1:] = indineqs[:, :-1]
+    polrepindineqs[:, 0] = indineqs[:, -1]
     polrepindineqs = -1*polrepindineqs
-    return polrepindineqs
 
-def posineqgen(n):
-    z = np.zeros(shape=(n,n+1), dtype=np.int)
-    z[:,1:] = np.identity(n)
-    return z
+    poldim = polrepindineqs.shape[0]
 
-def minineqgen(eqfname,n):
-    eqmat = redineq(eqfname)
-    posmat = posineqgen(n)
-    ineqs = eqmat.tolist() + posmat.tolist()
+    return polrepindineqs, poldim
+
+def posineqgen(poldim):
+    """
+    output: polymake compatibile positivity inequalities
+    """
+    posineq = np.zeros(shape=(poldim, poldim+1), dtype=np.int)
+    posineq[:, 1:] = np.identity(poldim)
+    return posineq
+
+def minineqgen(eqfname):
+    """
+    output: minineqs - combine the Kolmogorov Consistency inequalities
+                       with positivity inequalities
+    """
+    eqmat, poldim = redineq(eqfname)
+    posmat = posineqgen(poldim)
+    minineqs = eqmat.tolist() + posmat.tolist()
     #print sp.Matrix(ineqs)
-    return ineqs
+    return minineqs
 
-def writePolymakeScript(M):
-
+def runpolymakescript(minineqs, representation, polyproperty):
+    """
+    intermediate: filestring - polymake script with full-dimensional / minimal
+                         H-representation of the Kolmogorov Consistent
+                         polytope
+    output: polyout - output of polymake for given minineqs and
+                      polyproperty
+    """
     filestring = str("$Verbose::credits=0;\nuse application "
                      "\"polytope\";\nmy $ndineqs=new "
                      "Matrix<Rational>(%s);\nmy $nd=new "
-                     "Polytope<Rational>(INEQUALITIES=>$ndineqs);\nprint "
-                     "$nd->VERTICES;" % str(M))
+                     "Polytope<Rational>(%s=>$ndineqs);\nprint "
+                     "$nd->%s;" %
+                     (str(minineqs), representation, polyproperty))
 
-    return filestring
-
-def minineqrep(argv):
-    """
-    usage: python rrefmat.py C4.eq 8
-    """
-    eqfname = str(argv[0])
-    n = int(argv[1])
-    M = minineqgen(eqfname,n)
-    filestring = writePolymakeScript(M)
     scriptname = "kcscript.pl"
     fname = open(scriptname,'w')
     fname.write(filestring)
     fname.close()
-    vnd = subprocess.check_output(["polymake","--script","kcscript.pl"])
-    print vnd
-    return vnd
+    polyout = subprocess.check_output(["polymake", "--script",
+                                               "kcscript.pl"])
+
+    return polyout
+
+def booleverts(polyvertices):
+    """
+    remove rational vertices
+    """
+    def filtrat(line):
+        """
+        filter lines with '/'s indicating rational numbers
+        """
+        if "/" not in line:
+            vert = [int(x) for x in line.split(' ')]
+            return vert
+        else:
+            return
+
+    #filtverts = [[filtrat(x) for x in line.split(' ')]
+    #                         for line in polyvertices.split('\n')[:-1]]
+    filtverts = [filtrat(line)
+                             for line in polyvertices.split('\n')[:-1]]
+    filtverts = [l for l in filtverts if l is not None]
+    return filtverts
+
+def convert(srat):
+    """
+    convert rational number string to float
+    """
+    try:
+        return float(srat)
+    except ValueError:
+        num, denom = srat.split('/')
+        return float(num) / float(denom)
+
+def minineqrep(argv):
+    """
+    usage: python minineqrep.py C4.eq VERTICES
+    """
+    eqfname = str(argv[0])
+    polyproperty = str(argv[1])
+    minineqs = minineqgen(eqfname)
+    polyout = runpolymakescript(minineqs, "INEQUALITIES", polyproperty)
+
+    if polyproperty == "VERTICES":
+        boolepolyverts = booleverts(polyout)
+        volboole = runpolymakescript(boolepolyverts, "POINTS", "VOLUME")
+
+        volkolmogorov = runpolymakescript(minineqs,
+                                            "INEQUALITIES", "VOLUME")
+        vrat = convert(volboole) / convert(volkolmogorov)
+        #print "%s/%s ~ %0.6f" % (volboole, volkolmogorov, vrat)
+        print volboole
+        print volkolmogorov
+        print vrat
+
+    print "\n" + polyout
+    return polyout, vrat
 
 if __name__ == "__main__":
     #import doctest
