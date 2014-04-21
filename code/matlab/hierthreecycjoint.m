d4a57a2096a706503389e876ba4f0dffa2e64115
Cameron Smith <camrn86@gmail.com>
2014-04-21 16:41:37 -0400

    update joint hier three cyc

--- code/matlab/hierthreecycjoint.m
+++ code/matlab/hierthreecycjoint.m
@@ -1,8 +1,14 @@
-%% marginals
-% ostensible marginals correspond to probabilities
-% of pairwise states s
-% [00 01
-%  10 11]
+%% pairwise distributions associated to three-cycle graph
+
+%
+% probabilities of pairwise binary states
+% correspond to ostensible marginals
+% of p(x_1,x_2,x_3) for global state
+% X = (x_1,x_2,x_3) in {0,1}^3
+%
+% [p(00) p(01)
+%  p(10) p(11)]
+%
 
 % 1--2 marginal
 m(:,:,3) = [0.4 0.1;
@@ -16,21 +22,36 @@ m(:,:,1) = [0.4 0.1;
 m(:,:,2) = [0.1 0.4;
             0.4 0.1];
 
-% control distribution (x_4)
-% uniform over each controlled
+[x,y,z]=meshgrid(1:2,1:2,1:3);
+scatter3(x(:),y(:),z(:),90,m(:),'filled');
+view(gca,60,60);
+%slice(x,y,z,m,[1 2],[1 2],[1 2 3]);
+%isosurface(x,y,z,m,0.1)
+%isosurface(x,y,z,m,0.4)
+
+%% joint distribution
+
+%
+% p(x_1,x_2,x_3,x_4)
+% for global state
+% X = (x_1,x_2,x_3,x_4) in {1,2,3}^4
+%
+
+% distribution of control
+% random variable x_4
+% over each controlled
 % random variable (x_1,x_2,x_3)
 ph = [1/3 1/3 1/3];
 
-%% joint distribution
-% p(x_1,x_2,x_3,x_4)
-A = zeros([3 3 3 3]);
+dims = [3 3 3 3];
+A = zeros(dims);
 
-A(1:2,1:2,3,3) = m(:,:,3)*ph(3);
-A(3,1:2,1:2,1) = m(:,:,1)*ph(1);
-A(1:2,3,1:2,2) = m(:,:,2)*ph(2);
+A( 3 , 1:2, 1:2, 1) = m(:,:,1)*ph(1);
+A(1:2,  3,  1:2, 2) = m(:,:,2)*ph(2);
+A(1:2, 1:2,  3 , 3) = m(:,:,3)*ph(3);
 
-disp('joint distribution p(x_1,x_2,x_3,x_4)')
-dims = size(A);
+fprintf('\njoint distribution p(x_1,x_2,x_3,x_4)\n')
+fprintf('------------------------------\n')
 
 fprintf('x1x2x3x4 \t probability (including zeros)\n\n')
 for i = 1:dims(1)
@@ -43,7 +64,8 @@ for i = 1:dims(1)
     end
 end
 
-fprintf('\n\n\n')
+fprintf(['------------------------------\n\n'...
+         '------------------------------\n'])
 
 fprintf('x1x2x3x4 \t probability (non-zeros only)\n\n')
 for i = 1:dims(1)
@@ -58,9 +80,12 @@ for i = 1:dims(1)
     end
 end
 
-fprintf('\n\n\n')
+fprintf('------------------------------\n\n\n')
 
 %% conditional distributions
+
+disp('conditional distributions')
+fprintf('------------------------------\n')
 
 % 1--2 conditioned on x_3=3, x_4=3
 % p(x_1,x_2 | x_3=3, x_4=3) = p(x_1,x_2,x_3=3,x_4=3)/p(x_3=3,x_4=3)
@@ -70,14 +95,14 @@ disp(p12)
 
 % 2--3 conditioned on x_1=3, x_4=1
 % p(x_2,x_3 | x_1=3, x_4=1) = p(x_1=3,x_2,x_3,x_4=1)/p(x_1=3,x_4=1)
-
 p23 = reshape(A(3,:,:,1),3,3)./sum(sum(A(3,:,:,1)));
 disp('2--3 conditioned on x_1=3, x_4=1')
 disp(p23)
 
 % 1--3 conditioned on x_2=3, x_4=2
 % p(x_1,x_3 | x_2=3, x_4=2) = p(x_1,x_2=3,x_3,x_4=2)/p(x_2=3,x_4=2)
-
 p13 = reshape(A(:,3,:,2),3,3)./sum(sum(A(:,3,:,2)));
 disp('1--3 conditioned on x_2=3, x_4=2')
 disp(p13)
+
+fprintf('------------------------------\n')
